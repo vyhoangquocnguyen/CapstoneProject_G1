9a1577118d2bc92f6f8514750ae8420572fd25ac
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2021-08-05 21:45:52 -0400

    Update ultrasonic.cpp

--- Interfacing/BLE/ultrasonic.cpp
+++ Interfacing/BLE/ultrasonic.cpp
@@ -27,69 +27,64 @@ static uint16_t port;
 //#include <WiringPi.h>
 #include <sys/time.h>
 
-
-BLYNK_READ(V5)
-{    
 // Define GPIO to use on Pi ( wiringPI numbers )
 #define GPIO_TRIGGER=4 ;
 #define GPIO_ECHO=5 ;
-//Compensate distance from edge to sensor
-int COMP_SENSOR=0 ;
 //Variables used by gettimeofday
 struct timeval start, stop ; 
-// Set pins as output and input
-wiringPiSetup () ;
-pinMode (GPIO_TRIGGER, OUTPUT) ;
-pinMode (GPIO_ECHO, INPUT) ;
-//Set trigger to False (Low)
-digitalWrite (GPIO_TRIGGER,  0) ;
-//200 ms delay to start 
-delay (200) ;
-//Send 10us pulse to trigger
-
-digitalWrite (GPIO_TRIGGER,  1) ;
-delayMicroseconds (10) ;
-digitalWrite (GPIO_TRIGGER,  0) ;
-gettimeofday(&start, NULL) ;
-while (digitalRead (GPIO_ECHO)==0) 
-{
-    gettimeofday(&start, NULL) ;
-} 
-while (digitalRead (GPIO_ECHO)==1) 
-{
-    gettimeofday(&stop, NULL) ;
-} 
-
 // Calculate pulse length
 float TIME_DELTA, DISTANCE ;
-TIME_DELTA=(stop.tv_sec-start.tv_sec) + (stop.tv_usec-start.tv_usec) ;
 
-// Distance pulse travelled in that time is time multiplied by the speed of sound (cm/s)
-DISTANCE=TIME_DELTA * 34000 ;
+/***Functions****/
 
-// That was the distance there and back so halve the value
-DISTANCE=DISTANCE / 2 ;
-
-// Add compensation
-DISTANCE=DISTANCE + COMP_SENSOR ;
-
-// This command writes result to Virtual Pin (10) and convert usec to sec 
-Blynk.virtualWrite(5, DISTANCE  / 1000000) ;
+void ultrsetup()
+{
+// Set pins as output and input
+    wiringPiSetup () ;
+    pinMode (GPIO_TRIGGER, OUTPUT) ;
+    pinMode (GPIO_ECHO, INPUT) ;
+    //Set trigger to False (Low)
+    digitalWrite (GPIO_TRIGGER,  0) ;
+    //200 ms delay to start 
+    delay (200) ;
+}
+void level()
+{
+    //Send 10us pulse to trigger
+    digitalWrite (GPIO_TRIGGER,  1) ;
+    delayMicroseconds (10) ;
+    digitalWrite (GPIO_TRIGGER,  0) ;
+    gettimeofday(&start, NULL) ;
+    //reading sensor
+    while (digitalRead (GPIO_ECHO)==0) 
+    {
+        gettimeofday(&start, NULL) ;
+    } 
+    while (digitalRead (GPIO_ECHO)==1) 
+    {
+        gettimeofday(&stop, NULL) ;
+        TIME_DELTA=(stop.tv_sec-start.tv_sec) + (stop.tv_usec-start.tv_usec) ;
+        // Distance pulse travelled in that time is time multiplied by the speed of sound (cm/s)
+        DISTANCE=TIME_DELTA * 34000 ;
+        // That was the distance there and back so halve the value
+        DISTANCE=DISTANCE / 2 ;
+        // This command writes result to Virtual Pin (10) and convert usec to sec 
+        Blynk.virtualWrite(10, DISTANCE  / 1000000) ;
+    }
 
 }
 
 void setup()
 {
     Blynk.begin(auth, serv, port);
-
+    ultrsetup();
 }
 
 void loop()
 {
     Blynk.run();
+    level();
 }
-
-
 
 int main(int argc, char* argv[])
 {
@@ -104,4 +99,4 @@ int main(int argc, char* argv[])
     return 0;
 }
 
-sudo ./blynk --token=qlxUAUH_0vIwxvcb2W06yIsTKeE5c281
+//sudo ./blynk --token=qlxUAUH_0vIwxvcb2W06yIsTKeE5c281
