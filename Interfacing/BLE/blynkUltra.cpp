0f5133e603e36b8a21fe734ab69b90df5895c509
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2021-08-13 17:22:34 -0400

    Update blynkUltra.cpp

--- Interfacing/BLE/blynkUltra.cpp
+++ Interfacing/BLE/blynkUltra.cpp
@@ -45,6 +45,52 @@ BlynkSocket Blynk(_blynkTransport);
 #include <BlynkWidgets.h>
 
 BlynkTimer timer;
+#define TRUE (1==1)
+
+// HC-SR04 ultrasonic sensor on Raspberry pi 4
+#define TRIG 4
+#define ECHO 5
+
+static volatile long startTimeUsec;
+static volatile long endTimeUsec;
+double speedOfSoundMetersPerSecond = 340.29;
+
+void recordPulseLength() {
+    startTimeUsec = micros();
+    while (digitalRead(ECHO) == HIGH);
+    endTimeUsec = micros();
+}
+
+void setupUltrasonic() {
+    wiringPiSetup();
+    pinMode(TRIG, OUTPUT);
+    pinMode(ECHO, INPUT);
+
+    // TRIG pin must start LOW
+    // Initialize the sensor's trigger pin to low. If we don't pause
+    // after setting it to low, sometimes the sensor doesn't work right.
+    digitalWrite(TRIG, LOW);
+    delay(500); // .5 seconds
+}
+
+int getCM() {
+    // Send trig pulse
+    // Triggering the sensor for 10 microseconds will cause it to send out
+    // 8 ultrasonic (40Khz) bursts and listen for the echos.
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG, LOW);
+
+    int now = micros();
+    // Wait for echo start
+    // The sensor will raise the echo pin high for the length of time that it took
+    // the ultrasonic bursts to travel round trip.
+    while (digitalRead(ECHO) == LOW && micros() - now < 30000);
+    recordPulseLength();
+    long travelTimeUsec = endTimeUsec - startTimeUsec;
+    double distanceMeters = 100 * ((travelTimeUsec / 1000000.0) * 340.29) / 2;
+    return distanceMeters ;
+}
 
 // This function sends Arduino's up time every second to Virtual Pin (5).
 // In the app, Widget's reading frequency should be set to PUSH. This means
@@ -54,6 +100,7 @@ void myTimerEvent()
   // You can send any value at any time.
   // Please don't send more that 10 values per second.
   Blynk.virtualWrite(V5, millis() / 1000);
+  Blynk.vỉtualWrite(V2, getCM));
 }
 
 void setup()
