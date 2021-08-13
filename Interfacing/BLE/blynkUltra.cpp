0355b7aff1473f92defd52b10a0b2fef9c7a4010
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2021-08-13 16:53:38 -0400

    Update blynkUltra.cpp

--- Interfacing/BLE/blynkUltra.cpp
+++ Interfacing/BLE/blynkUltra.cpp
@@ -18,61 +18,66 @@ BlynkSocket Blynk(_blynkTransport);
 
 #define TRUE (1==1)
 
-// HC-SR04 ultrasonic sensor on Raspberry pi 4
-#define TRIG 4
-#define ECHO 5
+BLYNK_READ(10)
+{    
+  // Define GPIO to use on Pi ( wiringPI numbers )
+  int GPIO_TRIGGER=4 ;
+  int GPIO_ECHO=5 ;
 
-unsigned int uptime;      		// 1 second intervals
-unsigned int pinStatus;   		// status of BCM 17
-unsigned int lastpinStatus = 0; // to toggle
+  //Compensate distance from edge to sensor
+  int COMP_SENSOR=0 ;
 
-static volatile long startTimeUsec;
-static volatile long endTimeUsec;
-double speedOfSoundMetersPerSecond = 340.29;
+  //Variables used by gettimeofday
+  struct timeval start, stop ; 
 
+  // Set pins as output and input
+  wiringPiSetup () ;
+    pinMode (GPIO_TRIGGER, OUTPUT) ;
+    pinMode (GPIO_ECHO, INPUT) ;
 
-void recordPulseLength() {
-    startTimeUsec = micros();
-    while (digitalRead(ECHO) == HIGH);
-    endTimeUsec = micros();
-}
+  //Set trigger to False (Low)
+  digitalWrite (GPIO_TRIGGER,  0) ;
 
-void setup()
-{
-  wiringPiSetup();
-  pinMode(TRIG, OUTPUT);
-  pinMode(ECHO, INPUT);
-  // TRIG pin must start LOW
-  // Initialize the sensor's trigger pin to low. If we don't pause
-  // after setting it to low, sometimes the sensor doesn't work right.
-  digitalWrite(TRIG, LOW);
-  delay(500); // .5 seconds
+  //200 ms delay to start 
+  delay (200) ;
 
-}
+  //Send 10us pulse to trigger
+  digitalWrite (GPIO_TRIGGER,  1) ;
+  delayMicroseconds (10) ;
+  digitalWrite (GPIO_TRIGGER,  0) ;
+  gettimeofday(&start, NULL) ;
+  while (digitalRead (GPIO_ECHO)==0) 
+  {
+  gettimeofday(&start, NULL) ;
+  } 
+  while (digitalRead (GPIO_ECHO)==1) 
+  {
+  gettimeofday(&stop, NULL) ;
+  } 
 
-int getCM() {
-  // Send trig pulse
-  // Triggering the sensor for 10 microseconds will cause it to send out
-  // 8 ultrasonic (40Khz) bursts and listen for the echos.
-  digitalWrite(TRIG, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(TRIG, LOW);
-  int now = micros();
-  // Wait for echo start
-  // The sensor will raise the echo pin high for the length of time that it took
-  // the ultrasonic bursts to travel round trip.
-  while (digitalRead(ECHO) == LOW && micros() - now < 30000);
-  recordPulseLength();
-  long travelTimeUsec = endTimeUsec - startTimeUsec;
-  double distanceMeters = 100 * ((travelTimeUsec / 1000000.0) * 340.29) / 2;
-  return distanceMeters ;
+  // Calculate pulse length
+  float TIME_DELTA, DISTANCE ;
+  TIME_DELTA=(stop.tv_sec-start.tv_sec) + (stop.tv_usec-start.tv_usec) ;
+
+  // Distance pulse travelled in that time is time multiplied by the speed of sound (cm/s)
+  DISTANCE=TIME_DELTA * 34000 ;
+
+  // That was the distance there and back so halve the value
+  DISTANCE=DISTANCE / 2 ;
+
+  // Add compensation
+  DISTANCE=DISTANCE + COMP_SENSOR ;
+
+  // This command writes result to Virtual Pin (10) and convert usec to sec 
+  Blynk.virtualWrite(10, DISTANCE  / 1000000) ;
+
 }
 
 
 void loop()
 {
   Blynk.run();
-
+  Blynk.read(V2);
 }
 
 
@@ -87,23 +92,6 @@ int main(int argc, char* argv[])
   while(1) 
   {
   loop();
-  if (getCM() <= 3)
-  {
-    printf("Tank is full\n");
-    printf("Distance: %dcm\n", getCM());
-    delay(1000); // 0.5 second
-  }
-  else if (getCM() > 10)
-  {
-    printf("Tank is empty\n");
-    printf("Distance: %dcm\n", getCM());
-    delay(1000); // 0.5 second
-  }
-  else
-  {
-    printf("Safe zone, Distance: %dcm\n", getCM());
-    delay(1000);
-  }
   }
    return 0;
 }
