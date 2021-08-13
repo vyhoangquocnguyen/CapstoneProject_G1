6a8a2d91d677ee03f6c984e6a80631b9b4f6ffad
Vy Nguyễn <36269757+vyhoangquocnguyen@users.noreply.github.com>
2021-08-13 17:05:47 -0400

    Update blynkUltra.cpp

--- Interfacing/BLE/blynkUltra.cpp
+++ Interfacing/BLE/blynkUltra.cpp
@@ -1,6 +1,37 @@
-// Blynk "gp" numbers are BCM numbers, so gp17 is physical pin 11 
-// #define BLYNK_DEBUG
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  This example shows how value can be pushed from Arduino to
+  the Blynk App.
+
+  NOTE:
+  BlynkTimer provides SimpleTimer functionality:
+    http://playground.arduino.cc/Code/SimpleTimer
+
+  App project setup:
+    Value Display widget attached to Virtual Pin V5
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
 #define BLYNK_PRINT stdout
+
 #ifdef RASPBERRY
  #include <BlynkApiWiringPi.h>
 #else
@@ -9,90 +40,46 @@
 #include <BlynkSocket.h>
 #include <BlynkOptionsParser.h>
 
-#include <wiringPi.h>
-
 static BlynkTransportSocket _blynkTransport;
 BlynkSocket Blynk(_blynkTransport);
-
 #include <BlynkWidgets.h>
 
-#define TRUE (1==1)
+BlynkTimer timer;
 
-BLYNK_READ(10)
-{    
-  // Define GPIO to use on Pi ( wiringPI numbers )
-  int GPIO_TRIGGER=4 ;
-  int GPIO_ECHO=5 ;
-
-  //Compensate distance from edge to sensor
-  int COMP_SENSOR=0 ;
-
-  //Variables used by gettimeofday
-  struct timeval start, stop ; 
-
-  // Set pins as output and input
-  wiringPiSetup () ;
-    pinMode (GPIO_TRIGGER, OUTPUT) ;
-    pinMode (GPIO_ECHO, INPUT) ;
-
-  //Set trigger to False (Low)
-  digitalWrite (GPIO_TRIGGER,  0) ;
-
-  //200 ms delay to start 
-  delay (200) ;
-
-  //Send 10us pulse to trigger
-  digitalWrite (GPIO_TRIGGER,  1) ;
-  delayMicroseconds (10) ;
-  digitalWrite (GPIO_TRIGGER,  0) ;
-  gettimeofday(&start, NULL) ;
-  while (digitalRead (GPIO_ECHO)==0) 
-  {
-  gettimeofday(&start, NULL) ;
-  } 
-  while (digitalRead (GPIO_ECHO)==1) 
-  {
-  gettimeofday(&stop, NULL) ;
-  } 
-
-  // Calculate pulse length
-  float TIME_DELTA, DISTANCE ;
-  TIME_DELTA=(stop.tv_sec-start.tv_sec) + (stop.tv_usec-start.tv_usec) ;
-
-  // Distance pulse travelled in that time is time multiplied by the speed of sound (cm/s)
-  DISTANCE=TIME_DELTA * 34000 ;
-
-  // That was the distance there and back so halve the value
-  DISTANCE=DISTANCE / 2 ;
-
-  // Add compensation
-  DISTANCE=DISTANCE + COMP_SENSOR ;
-
-  // This command writes result to Virtual Pin (10) and convert usec to sec 
-  Blynk.virtualWrite(10, DISTANCE  / 1000000) ;
-
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  Blynk.virtualWrite(V5, millis() / 1000);
 }
 
+void setup()
+{
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
 
 void loop()
 {
   Blynk.run();
-  Blynk.read(V2);
+  timer.run(); // Initiates BlynkTimer
 }
-
-
 
 int main(int argc, char* argv[])
 {
   const char *auth, *serv;
   uint16_t port;
   parse_options(argc, argv, auth, serv, port);
-  Blynk.begin(auth, serv, port);
-  setup();
-  while(1) 
-  {
-  loop();
-  }
-   return 0;
-}
 
+  Blynk.begin(auth, serv, port);
+
+  setup();
+  while(true) {
+    loop();
+  }
+
+  return 0;
+}
